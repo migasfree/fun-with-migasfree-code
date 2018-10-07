83a5185f41cf7bb4747f0e815a67bc6665fa2b03
Alberto Gacias <alberto@migasfree.org>
2018-10-07 12:31:21 +0200

    Added acme-migasfree-external-actions package

---
+++ acme-migasfree-external-actions/usr/lib/firefox/defaults/pref/acme-migasfree-external-actions.js
@@ -0,0 +1,7 @@
+pref("network.protocol-handler.expose.mea", false);
+
+pref("network.protocol-handler.app.mea", "/usr/bin/acme-migasfree-external-actions");
+
+pref("network.protocol-handler.warn-external.mea", false);
+
+pref("network.protocol-handler.external.mea", true);
