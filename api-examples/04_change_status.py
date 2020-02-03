511ecb4ae2cc67c70d79940f82101b235882faf2
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-03 13:31:51 +0100

    Compatibility code python2/python3

--- api-examples/04_change_status.py
+++ api-examples/04_change_status.py
@@ -3,6 +3,7 @@
 
 # Change to status reserved the computers with label "ETQ-RED"
 
+from __future__ import print_function
 from migasfree_sdk.api import ApiToken
 
 
@@ -16,23 +17,30 @@ def main():
     try:
         attribute_id = api.id("attributes", {"prefix": prefix, "value": value})
     except:
-        print "ETQ-RED not exists"
+        print("{}-{} not exists".format(prefix, value))
         return
 
     computers = api.filter(
         "computers",
         {"sync_attributes__id": attribute_id, "ordering": "id"}
     )
+
+    i = 0
     for c in computers:
-        print c["name"], c["uuid"], c["status"]
+        print(c["name"], c["uuid"], c["status"])
         response = api.post(
             'computers/%i/status' % c["id"],
             {"status": "reserved"}
         )
         if api.is_ok(response.status_code):
-            print "Status changed to: %s" % response.json()["status"]
+            print("Status changed to: %s" % response.json()["status"])
         else:
-            print "Error: %s %s changing status" % (response.status_code, response.json())
+            print("Error: %s %s changing status" % (response.status_code, response.json()))
+        i += 1
+
+    if not i:
+        print("There are not computers with attribute {}-{}".format(prefix, value))
+
 
 if __name__ == "__main__":
     main()
